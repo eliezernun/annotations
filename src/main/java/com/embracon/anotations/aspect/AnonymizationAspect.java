aa5ffdf1121ae6a45636637ca43c5857952be146
Eliezer Nunes Rodrigues Junior <74334332+eliezernun@users.noreply.github.com>
2025-05-12 23:10:50 -0300

    1

--- src/main/java/com/embracon/anotations/aspect/AnonymizationAspect.java
+++ src/main/java/com/embracon/anotations/aspect/AnonymizationAspect.java
@@ -2,13 +2,18 @@ package com.embracon.anotations.aspect;
 
 import com.embracon.anotations.anotation.Anonymize;
 import com.embracon.anotations.service.AnonymizationService;
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Parameter;
-
+@RequiredArgsConstructor
+@Aspect
+@Component
 public class AnonymizationAspect {
 
     @Around("@annotation(org.springframework.web.bind.annotation.GetMapping) || " +
@@ -25,7 +30,7 @@ public class AnonymizationAspect {
 
                 // Codifica a entrada com base no tipo
                 args[i] = AnonymizationService.anonymize(args[i], type);
-            } else if (parameters[i].getType().getPackageName().startsWith("com.seuprojeto")) {
+            } else if (parameters[i].getType().getPackageName().startsWith("com.")) {
                 // Anonimiza propriedades dentro de objetos
                 Field[] fields = parameters[i].getType().getDeclaredFields();
                 for (Field field : fields) {
